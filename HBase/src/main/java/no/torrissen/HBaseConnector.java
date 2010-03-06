507cb18167a6fed0198af8d6130f4cc3d7c31c04
Tobias Torrissen <tobiast@macintosh.lan>
2010-03-06 14:32:37 +0100

    added put and create testcases

--- HBase/src/main/java/no/torrissen/HBaseConnector.java
+++ HBase/src/main/java/no/torrissen/HBaseConnector.java
@@ -9,11 +9,14 @@ package no.torrissen;
  */
 
 
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,29 +24,50 @@ import java.io.IOException;
 
 public class HBaseConnector {
 
-    public static Map retrieveMuppet(String muppetId) throws IOException {
-        HBaseConfiguration config =  new HBaseConfiguration();
-        config.set("hbase.zookeeper.quorum", "192.168.1.61");
 
-        HTable table = new HTable(config, "muppets");
-        Map muppet = new HashMap();
+    private static HTable table;
 
+    private static final Logger LOGGER = Logger.getLogger(HBaseConnector.class);
+    
+
+    public HBaseConnector(final String tableName, String ipAddress) throws IOException {
+       HBaseConfiguration config =  new HBaseConfiguration();
+       config.set("hbase.zookeeper.quorum", ipAddress);
+       table = new HTable(config, tableName);
+    }
+
+
+    /**
+     * Returns all values for a muppet.
+     * @param muppetId The muppet identifier
+     * @return a map containing all the keys and all the values
+     * @throws IOException if read from HBase fails
+     */
+    public  Map<String, String> retrieveMuppet(final String muppetId) throws IOException {
+
+        Map<String, String> muppet = new HashMap<String, String>();
         Get g = new Get(Bytes.toBytes(muppetId));
         Result r = table.get(g);
-        byte [] value = r.getValue(Bytes.toBytes("names"),Bytes.toBytes("fullname"));
 
-        String valueStr = Bytes.toString(value);
-        
-        System.out.println("GET: " + valueStr);
-
-       
-
+        KeyValue[] keyValues = r.raw();
+        for (KeyValue keyValue : keyValues) {
+            LOGGER.info("key: " + keyValue.toString() + " Value: " + Bytes.toString(keyValue.getValue()));
+            muppet.put(keyValue.toString(), Bytes.toString(keyValue.getValue()));
+        }
         return muppet;
     }
 
-    public static void main(String[] args) throws IOException {
-        Map muppet = HBaseConnector.retrieveMuppet("Kermit");
-        System.out.println(muppet.get("names:fullname"));
-        System.out.println(muppet.get("names:nickname"));
+
+
+    public  void createMuppet(final String muppetId, final Map<String, String> attributes) throws IOException {
+
+
+
+        Put p = new Put(Bytes.toBytes(muppetId));
+        p.add(Bytes.toBytes("names"), Bytes.toBytes("Andebynavn"),
+             Bytes.toBytes("Donald Duck"));
+             table.put(p);
+
     }
+
 }
---
+++ HBase/src/test/java/no/torrissen/HBaseConnectorTest.java
@@ -0,0 +1,59 @@
+package no.torrissen;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Unit test for the HBaseConnector
+ * User: tobiast
+ * Date: Mar 6, 2010
+ * Time: 2:17:44 PM
+ *
+ */
+public class HBaseConnectorTest extends TestCase {
+
+    private HBaseConnector myConnector;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tobiast
+ * Date: Mar 6, 2010
+ * Time: 2:17:44 PM
+     * @param testName name of the test case
+     * @throws java.io.IOException if connection fails.
+     */
+    public HBaseConnectorTest(String testName) throws IOException {
+        super(testName);
+        myConnector = new HBaseConnector("muppets", "10.0.0.26");
+    }
+
+
+    /**
+     * Tests read from the database.
+     *
+     * @throws Exception if connection fails
+     */
+    public void testRead() throws Exception {
+
+        Map<String, String> muppet = myConnector.retrieveMuppet("Kermit");
+        System.out.println(muppet.toString());
+    }
+
+    /**
+     * Tests write to the database.
+     *
+     * @throws java.io.IOException if connection fails
+     */
+    public void testWrite() throws IOException {
+
+        myConnector.createMuppet("Wukka Foobar", null);
+        Map<String, String> muppet2 = myConnector.retrieveMuppet("Wukka Foobar");
+
+    }
+
+}
+
+
