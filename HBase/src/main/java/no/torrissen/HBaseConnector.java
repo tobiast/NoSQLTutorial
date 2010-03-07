49eadb7e7243a7c0879b9c53691a37f9c6acb03a
Tobias Torrissen <tobiast@Tobias-Torrissens-MacBook-Pro.local>
2010-03-07 10:53:27 +0100

    Added metods to the startingpoint

--- HBase/src/main/java/no/torrissen/HBaseConnector.java
+++ HBase/src/main/java/no/torrissen/HBaseConnector.java
@@ -1,21 +1,13 @@
 package no.torrissen;
 
+
 /**
- * Created by IntelliJ IDEA.
+ * Class that puts stuff into an HBase instance.
  * User: tobiast
  * Date: Mar 2, 2010
- * Time: 8:48:40 PM
- * To change this template use File | Settings | File Templates.
+ * Time: 23:48:40 PM
  */
 
-
-import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 
 import java.util.HashMap;
@@ -26,6 +18,45 @@ public class HBaseConnector {
 
     private static final Logger LOGGER = Logger.getLogger(HBaseConnector.class);
 
-    
+
+    /**
+     * Constructor that also handles the connection.
+     *
+     * @param tableName the table (aka Map) to conenct to
+     * @param ipAddress the IP address of the Server
+     * @throws IOException if connection fails
+     */
+    public HBaseConnector(final String tableName, String ipAddress) throws IOException {
+
+    }
+
+
+    /**
+     * Stores a muppet in the DB.
+     * <p/>
+     * Iterates over the Map passed as parameter and inserts into the
+     *
+     * @param muppetId   the muppet identifier
+     * @param attributes a map containing all the muppet«s attributes. Keys must match the column families defined in the table
+     * @throws IOException if stuff blow up.
+     */
+    public void createMuppet(final String muppetId, final Map<String, Map<String, String>> attributes) throws IOException {
+
+
+    }
+
+
+    /**
+     * Returns all values for a muppet.
+     *
+     * @param muppetId The muppet identifier
+     * @return a map containing all the keys and all the values
+     * @throws IOException if read from HBase fails
+     */
+    public Map<String, String> retrieveMuppet(final String muppetId) throws IOException {
+
+
+        return new HashMap<String, String>();
+    }
 
 }
